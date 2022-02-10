af30ba5ac1774384ebb83325005c3a21e1643f60
Jathavaan Shankarr <jathavaan12@gmail.com>
2022-02-10 21:15:56 +0100

    Added all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
+  
+  function App() {
+    const [tasks, setTasks] = useState([
+      {
+          id: 1,
+          text: "Doctors appoitment",
+          day: "Feb 5th at 2:30 pm",
+          reminder: true
+      },
+      {
+          id: 2,
+          text: "Meeting at school",
+          day: "Feb 6th at 1:30 pm",
+          reminder: true
+      },
+      {
+          id: 3,
+          text: "Food shopping",
+          day: "Feb 5th at 2:30 pm",
+          reminder: false
+      }
+  ])
+
+  // Delete tasks
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      {
+        tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask}/> :
+        <p>No tasks to show</p>
+      }
     </div>
   );
 }
